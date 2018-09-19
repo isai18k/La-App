d7dda669263956ca4fa21ce349d92229d233bb59
isai_18k <isai.garcia.rys@gmail.com>
2018-09-19 17:07:45 -0500

    Initial Commit

---
+++ La AppTests/La_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  La_AppTests.swift
+//  La AppTests
+//
+//  Created by Jorge Isai Garcia Reyes on 19/09/18.
+//  Copyright © 2018 Jorge Isai Garcia Reyes. All rights reserved.
+//
+
+import XCTest
+@testable import La_App
+
+class La_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
