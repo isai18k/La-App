bb5faa462c14d05ac0a5ec923a8a96c1d837a026
isai_18k <isai.garcia.rys@gmail.com>
2018-09-19 17:54:55 -0500

    -ContactPickerViewController was added. -NSContactsUsageDescription was added.

--- La App/ViewController.swift
+++ La App/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import ContactsUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CNContactPickerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let contactStore = CNContactStore()
+        var contacts = [CNContact]()
+        let keys = [CNContactFormatter.descriptorForRequiredKeys(for: .fullName)]
+        let request = CNContactFetchRequest(keysToFetch: keys)
+        
+        do {
+            try contactStore.enumerateContacts(with: request) {
+                (contact, stop) in
+                // Array containing all unified contacts from everywhere
+                contacts.append(contact)
+            }
+        }
+        catch {
+            print("unable to fetch contacts")
+        }
+    }
+    
+    @IBAction func click_Contact(_ sender: Any) {
+        let cnPicker = CNContactPickerViewController()
+        cnPicker.delegate = self
+        self.present(cnPicker, animated: true, completion: nil)
     }
 
+    //MARK:- CNContactPickerDelegate Method
+    
+    func contactPicker(_ picker: CNContactPickerViewController, didSelect contacts: [CNContact]) {
+        contacts.forEach { contact in
+            for number in contact.phoneNumbers {
+                let phoneNumber = number.value
+                print("number is = \(phoneNumber)")
+            }
+        }
+    }
+    func contactPickerDidCancel(_ picker: CNContactPickerViewController) {
+        print("Cancel Contact Picker")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
